c99ba46d2c093510fdd3e03a5f03e20525b9ba22
kisaca-nsbe <seda6535ster@gmail.com>
2022-12-28 22:26:46 +0300

    new file:   hangisayidahabuyuk.py

---
+++ hangisayidahabuyuk.py
@@ -0,0 +1,10 @@
+while(True):
+    x=int(input('1.Sayıyı Giriniz:'))
+    y=int(input('2.Sayıyı Giriniz:'))
+    if(x>y):
+        print('1. sayı 2. sayıdan büyüktür')
+    elif(x<y):
+        print('2. sayı 1. sayıdan büyüktür')
+    else:
+        print('sayılar eşittir')
+
