e6102477c60c5862e28cc2fb1b3a508b541877ea
kisaca-nsbe <seda6535ster@gmail.com>
2022-12-28 21:01:51 +0300

    new file:   terst.py

---
+++ terst.py
@@ -0,0 +1,13 @@
+x = int(input('sayiyi giriniz: '))
+y =int(input('2. sayiyi giriniz: '))
+topla=x+y
+cikar=x-y
+carp=x*y
+bol=x/y
+karesi=x**2
+
+print('toplama:',topla)
+print('çıkarma:',cikar)
+print('çarpma:',carp)
+print('bölme:',bol)
+print('kare:',karesi)
